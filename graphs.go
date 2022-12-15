46388720eb92574513e12e7bc846d52cbd9ef694
Vladislav Savelyev <BashWhiteShaman@yandex.com>
2022-12-15 18:01:38 +0700

    Introduce types EdgeList and AdjacencyList

--- graphs.go
+++ graphs.go
@@ -7,7 +7,10 @@ import (
 	"github.com/savelyevvv/go-collections/stack"
 )
 
-func DFS(src string, graph map[string][]string) {
+type EdgeList [][2]string
+type AdjacencyList map[string][]string
+
+func DFS(src string, graph AdjacencyList) {
 	s := stack.New[string]()
 	s.Push(src)
 	seen := map[string]struct{}{src: {}}
@@ -24,7 +27,7 @@ func DFS(src string, graph map[string][]string) {
 	fmt.Println()
 }
 
-func BFS(src string, graph map[string][]string) {
+func BFS(src string, graph AdjacencyList) {
 	q := queue.New[string]()
 	q.Add(src)
 	seen := map[string]struct{}{src: {}}
@@ -41,14 +44,14 @@ func BFS(src string, graph map[string][]string) {
 	fmt.Println()
 }
 
-func DFSR(src string, graph map[string][]string) {
+func DFSR(src string, graph AdjacencyList) {
 	fmt.Print(src)
 	for _, neighbor := range graph[src] {
 		DFSR(neighbor, graph)
 	}
 }
 
-func HasPathDFS(src, dst string, graph map[string][]string) bool {
+func HasPathDFS(src, dst string, graph AdjacencyList) bool {
 	var rsl bool
 	s := stack.New[string]()
 	s.Push(src)
@@ -69,7 +72,7 @@ func HasPathDFS(src, dst string, graph map[string][]string) bool {
 	return rsl
 }
 
-func HasPathBFS(src, dst string, graph map[string][]string) bool {
+func HasPathBFS(src, dst string, graph AdjacencyList) bool {
 	var rsl bool
 	q := queue.New[string]()
 	q.Add(src)
@@ -90,7 +93,7 @@ func HasPathBFS(src, dst string, graph map[string][]string) bool {
 	return rsl
 }
 
-var g = map[string][]string{
+var g = AdjacencyList{
 	"a": {"b", "c", "d"},
 	"b": {"e", "f"},
 	"c": {"k", "l"},
@@ -104,4 +107,24 @@ var g = map[string][]string{
 	"k": {},
 	"l": {},
 	"m": {"l"},
+}
+
+var edges = EdgeList{
+	{"a", "b"},
+	{"b", "e"},
+	{"b", "f"},
+	{"e", "g"},
+	{"g", "f"},
+	{"f", "h"},
+	{"a", "c"},
+	{"c", "k"},
+	{"c", "l"},
+	{"m", "l"},
+	{"m", "k"},
+	{"k", "d"},
+	{"k", "i"},
+	{"k", "j"},
+	{"a", "d"},
+	{"d", "i"},
+	{"i", "j"},
 }
